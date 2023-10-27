077f6deb2fa6ba76ee11f7ca32d5cf1d0a030da0
@aryas1378 <aryas1378@gmail.com>
2023-10-27 09:30:29 +0100

    predict price method modified

--- predition.py
+++ predition.py
@@ -90,7 +90,7 @@ class Predict:
         
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         
-        model = LinearRegression()
+        model = DecisionTreeRegressor()
         model = model.fit(X_train, y_train)
         
         return model.predict(user_df)
